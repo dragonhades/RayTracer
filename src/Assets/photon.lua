a91f67ccdac7f8045831ab00daddcec04d891b01
Ziyang Shan <z7shan@uwaterloo.ca>
2018-12-03 00:09:49 -0500

    update lua

--- src/Assets/photon.lua
+++ src/Assets/photon.lua
@@ -5,7 +5,7 @@
 -- and lights have been modified accordingly.
 
 green = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 25, 1)
-gray = gr.material({0.5, 0.5, 0.5}, {0.1, 0.1, 0.1}, 15, 1)
+gray = gr.material({0.5, 0.5, 0.5}, {0.4, 0.4, 0.4}, 8, 1)
 dgray = gr.material({0.2, 0.2, 0.2}, {0.3, 0.3, 0.3}, 10, 1)
 yellow = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 25, 1)
 mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 25, 1)
@@ -21,9 +21,9 @@ scene = gr.node( 'scene' )
 scene:translate(0, -300, 0)
 scene:rotate('x', 20)
 
-s1 = gr.nh_sphere('s1', {0, 0, -400}, 100)
-scene:add_child(s1)
-s1:set_material(green)
+-- s1 = gr.nh_sphere('s1', {0, 0, -400}, 100)
+-- scene:add_child(s1)
+-- s1:set_material(green)
 
 s2 = gr.nh_sphere('s2', {200, 50, -100}, 150)
 scene:add_child(s2)
@@ -33,9 +33,9 @@ s2:set_material(green)
 -- scene:add_child(s3)
 -- s3:set_material(gray)
 
-b1 = gr.nh_box('b1', {50, -125, 150}, 100)
-scene:add_child(b1)
-b1:set_material(mat4)
+-- b1 = gr.nh_box('b1', {50, -125, 150}, 100)
+-- scene:add_child(b1)
+-- b1:set_material(mat4)
 
 -- c1 = gr.nh_cylinder('c1', {0, 0, 0}, 8, 300)
 -- scene:add_child(c1)
@@ -132,7 +132,7 @@ gr.render(scene, 'photon.png',
 		-- 256, 256,
 	-- 480, 270,
 	-- 640, 360,
-	-- 1280, 720,
-	3840, 2160,
+	1280, 720,
+	-- 3840, 2160,
 	  {0, -375, 1000}, {0, 0, -1}, {0, 1, 0}, 50,
-	  {0.3, 0.3, 0.3}, {white_light, white_lightl, white_lightr})
+	  {0.3, 0.3, 0.3}, {white_light})
--- src/Assets/texture.lua
+++ src/Assets/texture.lua
@@ -5,12 +5,12 @@
 -- and lights have been modified accordingly.
 
 green = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 25, 1)
-gray = gr.material({0.5, 0.5, 0.5}, {0.1, 0.2, 0.1}, 10, 1)
-dgray = gr.material({0.2, 0.2, 0.2}, {0.3, 0.3, 0.3}, 8, 1)
+gray = gr.material({0.5, 0.5, 0.5}, {0.1, 0.1, 0.1}, 15, 1)
+dgray = gr.material({0.2, 0.2, 0.2}, {0.3, 0.3, 0.3}, 10, 1)
 yellow = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 25, 1)
 mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 25, 1)
-blue = gr.material({0.1, 0.1, 0.3}, {0.1, 0.2, 0.1}, 3, 1)
-red = gr.material({1.0, 0.1, 0.3}, {0.1, 0.2, 0.1}, 3, 1)
+blue = gr.material({0.1, 0.1, 0.3}, {0.4, 0.4, 0.4}, 8, 1)
+red = gr.material({1.0, 0.1, 0.3}, {0.4, 0.4, 0.4}, 8, 1)
 glass = gr.material({0.0, 0.0, 0.2}, {0.8, 0.8, 0.8}, 50, 0.0)
 
 stone = gr.material({0.8, 0.7, 0.7}, {0.0, 0.0, 0.0}, 0, 1)
@@ -51,12 +51,12 @@ b1:set_material(mat4)
 -- scene:add_child(s5)
 -- s5:set_material(green)
 
-water = gr.mesh('water', 'plane.obj' )
-scene:add_child(water)
-water:set_material(glass)
-water:set_normalmap("water_normal.png")
-water:scale(500, 40, 500)
-water:translate(0, 50, 0)
+-- water = gr.mesh('water', 'plane.obj' )
+-- scene:add_child(water)
+-- water:set_material(glass)
+-- water:set_normalmap("water_normal.png")
+-- water:scale(500, 40, 500)
+-- water:translate(0, 50, 0)
 
 ground = gr.nh_box('ground', {0,0,0}, 1000)
 scene:add_child(ground)
@@ -115,9 +115,12 @@ testbr:translate(500, -200, -500)
 -- scene:add_child(steldodec)
 -- steldodec:translate(0, 100, 200)
 
-
-white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
-orange_light = gr.light({100.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
+white_lightlt = gr.light({-100.0, 500.0, 450.0}, {0.9, 0.9, 0.9}, {1, 0.003, 0})
+white_lightlb = gr.light({-100.0, 500.0, 350.0}, {0.9, 0.9, 0.9}, {1, 0.003, 0})
+white_light = gr.light({0.0, 500.0, 400}, {0.9, 0.9, 0.9}, {1, 0.0003, 0})
+white_lightrt = gr.light({100.0, 500.0, 450.0}, {0.9, 0.9, 0.9}, {1, 0.003, 0})
+white_lightrb = gr.light({100.0, 500.0, 350.0}, {0.9, 0.9, 0.9}, {1, 0.003, 0})
+orange_light = gr.light({100.0, 200.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
 
 gr.render(scene, 'texture.png', 
 	-- 256, 256,
@@ -126,9 +129,10 @@ gr.render(scene, 'texture.png',
 	-- 1280, 1280,
 	-- 2160, 2160,
 
-	480, 270,
+		-- 256, 256,
+	-- 480, 270,
 	-- 640, 360,
-	-- 1280, 720,
+	1280, 720,
 	-- 3840, 2160,
 	  {0, -375, 1000}, {0, 0, -1}, {0, 1, 0}, 50,
-	  {0.3, 0.3, 0.3}, {white_light, orange_light})
+	  {0.3, 0.3, 0.3}, {white_light})
